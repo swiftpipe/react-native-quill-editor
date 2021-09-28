73a7e3bfa33982beee1029f161a6e1e1bd11a332
Cao Khanh Nhat <knhat2014@gmail.com>
2021-09-28 16:46:51 +0700

    ^ ref

--- index.tsx
+++ index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Dimensions, Platform, ViewStyle } from 'react-native'
+import { Dimensions, Platform, ViewStyle, View } from 'react-native'
 import { WebView, WebViewMessageEvent } from 'react-native-webview'
 
 type Props = {
@@ -9,7 +9,12 @@ type Props = {
   onChange?: (html: string) => void
 }
 
-const Quill = (props: Props) => {
+const Quill = React.forwardRef((props: Props, ref) => {
+
+  const [height, $height] = React.useState(90);
+
+  const webviewRef = React.useRef(null);
+
   const options = JSON.stringify({
     placeholder: '请输入...',
     modules: {
@@ -19,26 +24,56 @@ const Quill = (props: Props) => {
     theme: 'snow',
   })
   const injectedJavaScriptBeforeContentLoaded = `window.options=${options}`
-  const injectedJavaScript = `document.querySelector('#editor').children[0].innerHTML="${props.defaultValue}"`
+  const injectedJavaScript = `document.querySelector('#editor').children[0].innerHTML="${props.defaultValue}";
+  `
 
   const onMessage = (e: WebViewMessageEvent) => {
     const data = JSON.parse(e.nativeEvent.data)
     if (data.type === 'onChange') {
       props.onChange(data.message)
+
+      if (data.height) {
+        if (data.height <= 200 && data.height > 90) {
+          $height(data.height)
+        }
+      }
     }
   }
 
+  React.useImperativeHandle(ref, () => ({
+    clearText: () => {
+      if (webviewRef.current) {  
+        webviewRef.current.postMessage(JSON.stringify({ type: 'clear' }));
+        $height(90)
+      }
+    },
+    setText: (value) => {
+      if (webviewRef.current) {
+        webviewRef.current.postMessage(JSON.stringify({ type: 'set', value }));
+      }
+    }
+  }));
+
+
+ 
   return (
-    <WebView
-      onMessage={onMessage}
-      source={Platform.OS === 'ios' ? require('./assets/quill.html') : { uri: 'file:///android_asset/quill.html' }}
-      javaScriptEnabled
-      injectedJavaScriptBeforeContentLoaded={injectedJavaScriptBeforeContentLoaded}
-      injectedJavaScript={injectedJavaScript}
-      style={{ height: Dimensions.get('window').height - 42, width: Dimensions.get('window').width, ...props.style }}
-    />
+    <View style={{ height }}>
+      <WebView
+        onMessage={onMessage}
+        ref={webviewRef}
+        source={Platform.OS === 'ios' ? require('./assets/quill.html') : { uri: 'file:///android_asset/quill.html' }}
+        javaScriptEnabled
+        injectedJavaScriptBeforeContentLoaded={injectedJavaScriptBeforeContentLoaded}
+        injectedJavaScript={injectedJavaScript}
+        bounces={false}
+        scrollEnabled={false}
+        hideKeyboardAccessoryView
+        automaticallyAdjustContentInsets={false}
+        style={{ width: Dimensions.get('window').width }}
+      />
+    </View>
   )
-}
+});
 
 Quill.defaultProps = {
   style: {},
@@ -47,4 +82,4 @@ Quill.defaultProps = {
   options: {},
 }
 
-export default Quill
+export default  Quill
